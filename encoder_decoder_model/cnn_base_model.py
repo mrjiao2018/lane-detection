66bc83388cdc0d884ee0d785ba434dc0dd36458d
mrjiao <mrjiao2018@163.com>
2018-12-06 10:57:24 +0800

    packing cnn_base_model

--- encoder_decoder_model/cnn_base_model.py
+++ encoder_decoder_model/cnn_base_model.py
@@ -196,36 +196,55 @@ class CNNBaseModel(object):
         :param name:
         :return:
         """
+        shape = input_data.get_shape().as_list()
+        ndims = len(shape)
+        assert ndims in [2, 4]
 
-    @staticmethod
-    def instance_norm(input_data, epsilon=1e-5, data_format='NHWC', use_affine=True, name=None):
-        """
+        mean, var = tf.nn.moments(input_data, list(range(1, len(shape))), keep_dims=True)
 
-        :param inputdata:
-        :param epsilon:
-        :param data_format:
-        :param use_affine:
-        :param name:
-        :return:
-        """
+        if data_format == 'NCHW':
+            channnel = shape[1]
+            new_shape = [1, channnel, 1, 1]
+        else:
+            channnel = shape[-1]
+            new_shape = [1, 1, 1, channnel]
+        if ndims == 2:
+            new_shape = [1, channnel]
+
+        if use_bias:
+            beta = tf.get_variable('beta', [channnel], initializer=tf.constant_initializer())
+            beta = tf.reshape(beta, new_shape)
+        else:
+            beta = tf.zeros([1] * ndims, name='beta')
+        if use_scale:
+            gamma = tf.get_variable('gamma', [channnel], initializer=tf.constant_initializer(1.0))
+            gamma = tf.reshape(gamma, new_shape)
+        else:
+            gamma = tf.ones([1] * ndims, name='gamma')
+
+        return tf.nn.batch_normalization(input_data, mean, var, beta, gamma, epsilon, name=name)
 
     @staticmethod
     def dropout(input_data, keep_prob, noise_shape=None, name=None):
         """
 
         :param input_data:
-        :param keep_prob:
-        :param noise_shape:
+        :param keep_prob:A scalar Tensor with the same type as x. The probability that each element is kept.
+        :param noise_shape:A 1-D Tensor of type int32, representing the shape for randomly generated keep/drop flags
         :param name:
-        :return:
+        :return: A Tensor of the same shape of x
         """
+        return tf.nn.dropout(input_data, keep_prob=keep_prob, noise_shape=noise_shape, name=name)
 
     @staticmethod
-    def fully_connect(inputdata, out_dim, w_init=None, b_init=None,
+    def fully_connect(input_data, out_dim, w_init=None, b_init=None,
                      use_bias=True, name=None):
         """
+        Fully-Connected layer, takes a N>1D tensor and returns a 2D tensor.
+        It is an equivalent of `tf.layers.dense` except for naming conventions.
+        Dense layer implements the operation: outputs = activation(inputs * kernel + bias)
 
-        :param inputdata:
+        :param input_data:
         :param out_dim:
         :param w_init:
         :param b_init:
@@ -233,16 +252,32 @@ class CNNBaseModel(object):
         :param name:
         :return:
         """
+        shape = input_data.get_shape().as_list()[1:]
+        if None not in shape:
+            inputdata = tf.reshape(input_data, [-1, int(np.prod(shape))])
+        else:
+            inputdata = tf.reshape(input_data, tf.stack([tf.shape(input_data)[0], -1]))
+
+        if w_init is None:
+            w_init = tf.contrib.layers.variance_scaling_initializer()
+        if b_init is None:
+            b_init = tf.constant_initializer()
+
+        ret = tf.layers.dense(inputs=inputdata, activation=lambda x: tf.identity(x, name='output'),
+                              use_bias=use_bias, name=name,
+                              kernel_initializer=w_init, bias_initializer=b_init,
+                              trainable=True, units=out_dim)
+        return ret
 
     @staticmethod
-    def deconv2d(inputdata, out_channel, kernel_size, padding='SAME',
+    def deconv2d(input_data, out_channel, kernel_size, padding='SAME',
                  stride=1, w_init=None, b_init=None,
                  use_bias=True, activation=None, data_format='channels_last',
                  trainable=True, name=None):
         """
+        Packing the tensorflow conv2d function.
 
-
-        :param inputdata:
+        :param input_data:
         :param out_channel:
         :param kernel_size:
         :param padding:
@@ -256,7 +291,74 @@ class CNNBaseModel(object):
         :param name:
         :return:
         """
+        with tf.variable_scope(name):
+            in_shape = input_data.get_shape().as_list()
+            channel_axis = 3 if data_format == 'channels_last' else 1
+            in_channel = in_shape[channel_axis]
+            assert in_channel is not None, "[Deconv2D] Input cannot have unknown channel!"
+
+            padding = padding.upper()
+
+            if w_init is None:
+                w_init = tf.contrib.layers.variance_scaling_initializer()
+            if b_init is None:
+                b_init = tf.constant_initializer()
+
+            ret = tf.layers.conv2d_transpose(inputs=input_data, filters=out_channel,
+                                             kernel_size=kernel_size,
+                                             strides=stride, padding=padding,
+                                             data_format=data_format,
+                                             activation=activation, use_bias=use_bias,
+                                             kernel_initializer=w_init,
+                                             bias_initializer=b_init, trainable=trainable,
+                                             name=name)
+        return ret
 
     @staticmethod
-    def dilation_conv():
-        return None
+    def dilation_conv(input_data, k_size, out_dims, rate, padding='SAME',
+                      w_init=None, b_init=None, use_bias=False, name=None):
+        """
+
+        :param input_data:
+        :param k_size:
+        :param out_dims:
+        :param rate:
+        :param padding:
+        :param w_init:
+        :param b_init:
+        :param use_bias:
+        :param name:
+        :return:
+        """
+        with tf.variable_scope(name):
+            in_shape = input_data.get_shape().as_list()
+            in_channel = in_shape[3]
+            assert in_channel is not None, "[Conv2D] Input cannot have unknown channel!"
+
+            padding = padding.upper()
+
+            if isinstance(k_size, list):
+                filter_shape = [k_size[0], k_size[1]] + [in_channel, out_dims]
+            else:
+                filter_shape = [k_size, k_size] + [in_channel, out_dims]
+
+            if w_init is None:
+                w_init = tf.contrib.layers.variance_scaling_initializer()
+            if b_init is None:
+                b_init = tf.constant_initializer()
+
+            w = tf.get_variable('W', filter_shape, initializer=w_init)
+            b = None
+
+            if use_bias:
+                b = tf.get_variable('b', [out_dims], initializer=b_init)
+
+            conv = tf.nn.atrous_conv2d(value=input_data, filters=w, rate=rate,
+                                       padding=padding, name='dilation_conv')
+
+            if use_bias:
+                ret = tf.add(conv, b)
+            else:
+                ret = conv
+
+        return ret
